7b9354cec6b050f3ad44acdcf46b9d32dea564c9
ValentinBrizard <121931010+ValentinBrizard@users.noreply.github.com>
2024-08-02 10:15:01 +0200

    Create navbar.php

---
+++ navbar.php
@@ -0,0 +1,15 @@
+<!-- navbar.php -->
+<nav class="navbar">
+    <div class="navbar-container">
+        <ul class="menu">
+            <li class="menu-item dropdown">
+                <a href="#" class="menu-link">Affinités</a>
+                <div class="dropdown-content">
+                    <a href="forestier.html">Forestier</a>
+                    <a href="#">Berserker</a>
+                </div>
+            </li>
+            <!-- Ajoute d'autres liens de navigation si nécessaire -->
+        </ul>
+    </div>
+</nav>
