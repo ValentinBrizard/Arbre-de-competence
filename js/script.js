42858fff878cd2f1902628a23c034951239a073a
ValentinBrizard <121931010+ValentinBrizard@users.noreply.github.com>
2024-08-02 09:59:40 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const dropdowns = document.querySelectorAll('.dropdown');
     dropdowns.forEach(dropdown => {
         dropdown.addEventListener('click', function(event) {
-            event.preventDefault();
+          //  event.preventDefault();
             let dropdownContent = this.querySelector('.dropdown-content');
             dropdownContent.style.display = dropdownContent.style.display === 'block' ? 'none' : 'block';
         });
